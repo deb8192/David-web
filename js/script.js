51b07b03926339bd227e9604bbf2e1402f90f0fa
Débora Galdeano González <deb8192@gmail.com>
2020-09-20 13:13:57 +0200

    interconexión entre páginas de distintos idiomas hecha (falta crear páginas en catalan)

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
 const _true = true,
 _false = false,
-_midWidth = 720;
+
+_midWidth = 720,
+_zero = 0,
+_one = 1,
+_two = 2,
+
+_spanish = "es",
+_english = "en",
+_catalan = "cat",
+_spanishSel = 0,
+_englishSel = 1,
+_catalanSel = 2,
+
+//change .html to .php
+_spanishPages = ["index.html", "curriculum.html", "fotos.html", "videobook.html", "noticias.html", "sobre-mi.html", "contacto.html"],
+_englishPages = ["home.html", "cv.html", "pictures.html", "videobook.html", "news.html", "about-me.html", "contact.html"],
+_catalanPages = ["index.html", "curriculum.html", "fotos.html", "videobook.html", "noticies.html", "sobre-mi.html", "contacte.html"];
+
 
 function showSocial(checkBox)
 {
-    let styleLeft = "",
+    var styleLeft = "",
     checkValPerc = "-36%",
     nonCheckValPerc = "-100%";
     if(checkBox.checked == true)
@@ -20,7 +37,7 @@ function showSocial(checkBox)
 
 function checkSocial()
 {
-    let width = (window.innerWidth > 0) ? window.innerWidth : screen.width,
+    var width = (window.innerWidth > 0) ? window.innerWidth : screen.width,
     defaultLeftVal = "0%";
     if(width >= _midWidth)
     {
@@ -30,4 +47,286 @@ function checkSocial()
     {
         showSocial(document.querySelector('.checkMenu'));
     }
+}
+
+function changeLink(button)
+{
+    var separator = '/',
+    srcSplit = window.location.pathname.split(separator),
+    result = window.location.pathname;
+    
+    if(srcSplit.length > _zero)
+    {   
+        switch(button.name)
+        {
+            case _spanish:
+                result = changeLanguageFolder(srcSplit, _spanish);
+                found = _true;
+                break;
+
+            case _english:
+                result = changeLanguageFolder(srcSplit, _english);
+                found = _true;
+                break;
+
+            case _catalan:
+                result = changeLanguageFolder(srcSplit, _catalan);
+                found = _true;
+                break;
+        }
+    }
+    window.location.replace(result);
+}
+
+function changeLanguageFolder(path, lang)
+{
+    var result,
+    i = 0;
+    found = _false; 
+    while(i < path.length && !found)
+    {
+        if(i == path.length - _two)
+        {
+            if(lang == _spanish)
+            {
+                if(path[i] == _english || path[i] == _catalan)
+                {
+                    path.splice(i, _one);
+                    path[i] = changeEquivalentLink(path[i], _spanishSel); 
+                    found = _true;
+                }
+            }
+            else
+            {
+                if(lang == _english)
+                {
+                    if(path[i] == _catalan)
+                    {                    
+                        path[i] = lang;
+                        path[i + _one] = changeEquivalentLink(path[i + _one], _englishSel); 
+                    }
+                    else if(path[i] != _english)
+                    {
+                        i++;
+                        path.splice(i, _zero, lang);
+                        path[i + _one] = changeEquivalentLink(path[i + _one], _englishSel); 
+                    }
+                }
+                else if(lang == _catalan)
+                {
+                    if(path[i] == _english)
+                    {                    
+                        path[i] = lang;
+                        path[i + _one] = changeEquivalentLink(path[i + _one], _catalanSel);
+                    }
+                    else if(path[i] != _catalan)
+                    {
+                        i++;
+                        path.splice(i, _zero, lang);
+                        path[i + _one] = changeEquivalentLink(path[i + _one], _catalanSel);
+                    }
+                }
+                found = _true;
+            }
+        }
+        i++;
+    }
+    
+    result = path.join('/');
+    return result;
+}
+
+function changeEquivalentLink(changingElement, newLang)
+{
+    switch(newLang)
+    {
+        case 0:
+            {
+                switch(changingElement)
+                {
+                    case _englishPages[0]:
+                        changingElement = _spanishPages[0];
+                        break;
+                        
+                    case _englishPages[1]:
+                        changingElement = _spanishPages[1];
+                        break;
+                        
+                    case _englishPages[2]:
+                        changingElement = _spanishPages[2];
+                        break;
+                        
+                    case _englishPages[3]:
+                        changingElement = _spanishPages[3];
+                        break;
+
+                    case _englishPages[4]:
+                        changingElement = _spanishPages[4];
+                        break;
+                        
+                    case _englishPages[5]:
+                        changingElement = _spanishPages[5];
+                        break;
+                        
+                    case _englishPages[6]:
+                        changingElement = _spanishPages[6];
+                        break;
+
+                    case _catalanPages[0]:
+                        changingElement = _spanishPages[0];
+                        break;
+                        
+                    case _catalanPages[1]:
+                        changingElement = _spanishPages[1];
+                        break;
+                        
+                    case _catalanPages[2]:
+                        changingElement = _spanishPages[2];
+                        break;
+                        
+                    case _catalanPages[3]:
+                        changingElement = _spanishPages[3];
+                        break;
+
+                    case _catalanPages[4]:
+                        changingElement = _spanishPages[4];
+                        break;
+                        
+                    case _catalanPages[5]:
+                        changingElement = _spanishPages[5];
+                        break;
+                        
+                    case _catalanPages[6]:
+                        changingElement = _spanishPages[6];
+                        break;
+                }
+            }
+            break;
+
+        case 1:
+            {
+                switch(changingElement)
+                {
+                    case _spanishPages[0]:
+                        changingElement = _englishPages[0];
+                        break;
+                        
+                    case _spanishPages[1]:
+                        changingElement = _englishPages[1];
+                        break;
+                        
+                    case _spanishPages[2]:
+                        changingElement = _englishPages[2];
+                        break;
+                        
+                    case _spanishPages[3]:
+                        changingElement = _englishPages[3];
+                        break;
+
+                    case _spanishPages[4]:
+                        changingElement = _englishPages[4];
+                        break;
+                        
+                    case _spanishPages[5]:
+                        changingElement = _englishPages[5];
+                        break;
+                        
+                    case _spanishPages[6]:
+                        changingElement = _englishPages[6];
+                        break;
+
+                    case _catalanPages[0]:
+                        changingElement = _englishPages[0];
+                        break;
+                        
+                    case _catalanPages[1]:
+                        changingElement = _englishPages[1];
+                        break;
+                        
+                    case _catalanPages[2]:
+                        changingElement = _englishPages[2];
+                        break;
+                        
+                    case _catalanPages[3]:
+                        changingElement = _englishPages[3];
+                        break;
+
+                    case _catalanPages[4]:
+                        changingElement = _englishPages[4];
+                        break;
+                        
+                    case _catalanPages[5]:
+                        changingElement = _englishPages[5];
+                        break;
+                        
+                    case _catalanPages[6]:
+                        changingElement = _englishPages[6];
+                        break;
+                }
+            }
+            break;
+        case 2:
+            {
+                switch(changingElement)
+                {
+                    case _englishPages[0]:
+                        changingElement = _catalanPages[0];
+                        break;
+                        
+                    case _englishPages[1]:
+                        changingElement = _catalanPages[1];
+                        break;
+                        
+                    case _englishPages[2]:
+                        changingElement = _catalanPages[2];
+                        break;
+                        
+                    case _englishPages[3]:
+                        changingElement = _catalanPages[3];
+                        break;
+
+                    case _englishPages[4]:
+                        changingElement = _catalanPages[4];
+                        break;
+                        
+                    case _englishPages[5]:
+                        changingElement = _catalanPages[5];
+                        break;
+                        
+                    case _englishPages[6]:
+                        changingElement = _catalanPages[6];
+                        break;
+
+                    case _spanishPages[0]:
+                        changingElement = _catalanPages[0];
+                        break;
+                        
+                    case _spanishPages[1]:
+                        changingElement = _catalanPages[1];
+                        break;
+                        
+                    case _spanishPages[2]:
+                        changingElement = _catalanPages[2];
+                        break;
+                        
+                    case _spanishPages[3]:
+                        changingElement = _catalanPages[3];
+                        break;
+
+                    case _spanishPages[4]:
+                        changingElement = _catalanPages[4];
+                        break;
+                        
+                    case _spanishPages[5]:
+                        changingElement = _catalanPages[5];
+                        break;
+                        
+                    case _spanishPages[6]:
+                        changingElement = _catalanPages[6];
+                        break;
+                }
+            }
+            break;
+    }
+    return changingElement;
 }
