1c0efde2015c8ba3171a7cd975307f4acd58f616
Débora Galdeano González <deb8192@gmail.com>
2020-11-22 20:41:28 +0100

    Base de datos preparada para añadir las fotos (y usuarios, permisos, etc.)

--- admin/db/DW-database.sql
+++ admin/db/DW-database.sql
@@ -1,3 +1,5 @@
+/************** Database creation **************/
+
 CREATE DATABASE davidVallsWeb;
 
 CREATE TABLE PicSections(
@@ -7,14 +9,30 @@ CREATE TABLE PicSections(
     PRIMARY KEY(id)
 ) ENGINE = innodb;
 
+CREATE TABLE PicSizes(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    sizeName VARCHAR(20),
+
+    PRIMARY KEY(id)
+)   ENGINE = innodb;
+
 CREATE TABLE Pictures(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     title VARCHAR(20),
     urlPic VARCHAR(100),
+    picDescription VARCHAR(100),
+    picDescriptionEng VARCHAR(100),
+    picDescriptionCat VARCHAR(100),
     picSectionID INT UNSIGNED,
+    picSizeID INT UNSIGNED,
 
     PRIMARY KEY(id),
-    FOREIGN KEY(picSectionID) REFERENCES PicSections(id)
+    FOREIGN KEY(picSectionID) REFERENCES PicSections(id) 
+        ON DELETE SET NULL
+        ON UPDATE CASCADE,
+    FOREIGN KEY(picSizeID) REFERENCES PicSizes(id)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE
 ) ENGINE = innodb;
 
 CREATE TABLE UserPrivileges(
@@ -32,4 +50,28 @@ CREATE TABLE Users(
 
     PRIMARY KEY(email),
     FOREIGN KEY(privilegeID) REFERENCES UserPrivileges(id)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE
 ) ENGINE = innodb;
+/************** !Database creation **************/
+
+/************* Setting tables values *************/
+
+INSERT INTO PicSections (sectionName) 
+    VALUES  ("MainSlider"),
+            ("PicturesGrid");
+
+INSERT INTO PicSizes (sizeName) 
+    VALUES  ("Small"),
+            ("Medium"),
+            ("Large");
+
+INSERT INTO Pictures (title, urlPic, picDescription, picDescriptionEng, picDescriptionCat, picSectionID, picSizeID)
+    VALUES  ("MainSlide.jpg", "images/pictures/mainSlider/large/mainSlider.jpg", "Foto grande del slider de la página inicial de David Valls Gomez Web", "David Valls Gomez Web homepage's main slider large picture", "Foto gran del slider de la pagina inicial de David Valls Gomez Web", 0, 2),
+            ("MainSlide.jpg", "images/pictures/mainSlider/large/mainSlider.jpg", "Foto grande del slider de la página inicial de David Valls Gomez Web", "David Valls Gomez Web homepage's main slider large picture", "Foto gran del slider de la pagina inicial de David Valls Gomez Web", 0, 2),;
+
+INSERT INTO UserPrivileges (privilegeName)
+    VALUES  ("Super Admin"),
+            ("Admin"),
+            ("Editor"),
+            ("Author");
